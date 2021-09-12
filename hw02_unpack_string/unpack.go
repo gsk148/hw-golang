274daf37795116865caa783d561eff15fad68616
Anton Shelkov <shelkovaf@mos.ru>
2021-09-12 22:47:09 +0300

    implement unpack string function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(str string) (string, error) {
 	// Place your code here.
-	return "", nil
+	if len(strings.TrimSpace(str)) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(rune(str[0])) {
+		return "", ErrInvalidString
+	}
+
+	var b strings.Builder
+	for i, v := range str {
+		next := i + 1
+
+		if unicode.IsDigit(v) {
+			if len(str) != next {
+				if unicode.IsDigit(rune(str[next])) {
+					return "", ErrInvalidString
+				}
+			}
+			continue
+		}
+
+		if len(str) == next {
+			b.WriteRune(v)
+			continue
+		}
+
+		if unicode.IsDigit(rune(str[next])) {
+			d := str[next]
+
+			count, _ := strconv.Atoi(string(d))
+			calculated := strings.Repeat(string(str[i]), count)
+			println(calculated)
+
+			b.WriteString(calculated)
+		} else {
+			b.WriteRune(v)
+		}
+	}
+	res := b.String()
+
+	return res, nil
 }
