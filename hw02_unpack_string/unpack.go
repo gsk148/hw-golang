588d549774aac768a36bc5895aa452d30f5a09f2
Anton Shelkov <shelkovaf@mos.ru>
2021-09-13 23:23:58 +0300

    refactor unpack function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,46 +10,27 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	// Place your code here.
-	if len(strings.TrimSpace(str)) == 0 {
-		return "", nil
-	}
-
-	if unicode.IsDigit(rune(str[0])) {
-		return "", ErrInvalidString
-	}
-
+	var value string
 	var b strings.Builder
-	for i, v := range str {
-		next := i + 1
 
+	for _, v := range str {
 		if unicode.IsDigit(v) {
-			if len(str) != next {
-				if unicode.IsDigit(rune(str[next])) {
-					return "", ErrInvalidString
-				}
+			if value == "" {
+				return "", ErrInvalidString
 			}
+
+			d, _ := strconv.Atoi(string(v))
+			b.WriteString(strings.Repeat(value, d))
+			value = ""
+
 			continue
 		}
 
-		if len(str) == next {
-			b.WriteRune(v)
-			continue
-		}
-
-		if unicode.IsDigit(rune(str[next])) {
-			d := str[next]
-
-			count, _ := strconv.Atoi(string(d))
-			calculated := strings.Repeat(string(str[i]), count)
-			println(calculated)
-
-			b.WriteString(calculated)
-		} else {
-			b.WriteRune(v)
-		}
+		b.WriteString(value)
+		value = string(v)
 	}
-	res := b.String()
 
-	return res, nil
+	b.WriteString(value)
+
+	return b.String(), nil
 }
