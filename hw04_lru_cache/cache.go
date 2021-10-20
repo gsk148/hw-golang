983adfdc52e132c463c6591f8620d64c59abab38
Anton Shelkov <shelkovaf@mos.ru>
2021-10-20 09:33:00 +0300

    implement cache and add tests

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -27,4 +25,41 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if i, ok := c.items[key]; ok {
+		i.Value = &cacheItem{
+			key:   string(key),
+			value: value,
+		}
+		c.queue.MoveToFront(i)
+		return true
+	}
+
+	c.queue.PushFront(&cacheItem{
+		key:   string(key),
+		value: value,
+	})
+	c.items[key] = c.queue.Front()
+
+	if c.capacity < c.queue.Len() {
+		lru := c.queue.Back()
+		delete(c.items, Key(lru.Value.(*cacheItem).key))
+		c.queue.Remove(lru)
+	}
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if e, ok := c.items[key]; ok {
+		c.queue.MoveToFront(e)
+		return e.Value.(*cacheItem).value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue = NewList()
 }
